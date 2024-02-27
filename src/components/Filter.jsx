205e19c8faeed8139983beb20f84c19e63bc1e58
Faizan <sfaizan78692@gmail.com>
2024-02-27 15:14:47 +0530

    button text color change

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -76,8 +76,8 @@ function Filter() {
                     ))}
                 </div>
                 <div className='flex gap-4 mt-4'>
-                    <div className={`${genre ? 'block': 'hidden'} cursor-pointer p-2 rounded-xl bg-[#91c8ff]`} onClick={applyFilter}>Apply</div>
-                    <div className={`${genre ? 'block': 'hidden'} cursor-pointer p-2 rounded-xl bg-[#91c8ff]`} onClick={clearFilter}>Clear</div>
+                    <div className={`${genre ? 'block': 'hidden'} cursor-pointer p-2 rounded-xl text-white bg-[#91c8ff]`} onClick={applyFilter}>Apply</div>
+                    <div className={`${genre ? 'block': 'hidden'} cursor-pointer p-2 rounded-xl text-white bg-[#91c8ff]`} onClick={clearFilter}>Clear</div>
                 </div>
             </div>
         </div>
